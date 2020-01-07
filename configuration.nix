6b9e96f638fc934de6c04190b5902194f9ac8ebe
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-01-06 19:50:35 -0500

    Add modules for stage-1 display init

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,39 @@ in
 
   boot.kernelPackages = pkgs.linuxPackages_pinebookpro;
 
+  # This list of modules is not entirely minified, but represents
+  # a set of modules that is required for the display to work in stage-1.
+  # Further minification can be done, but requires trial-and-error mainly.
+  boot.initrd.kernelModules = [
+    # Rockchip modules
+    "rockchip_rga"
+    "rockchip_saradc"
+    "rockchip_thermal"
+    "rockchipdrm"
+
+    # GPU/Display modules
+    "analogix_dp"
+    "cec"
+    "drm"
+    "drm_kms_helper"
+    "dw_hdmi"
+    "dw_mipi_dsi"
+    "gpu_sched"
+    "panel_simple"
+    "panfrost"
+    "pwm_bl"
+
+    # USB / Type-C related modules
+    "fusb302"
+    "tcpm"
+    "typec"
+
+    # Misc. modules
+    "cw2015_battery"
+    "gpio_charger"
+    "rtc_rk808"
+  ];
+
   sdImage = {
     manipulateImageCommands = ''
       (PS4=" $ "; set -x
