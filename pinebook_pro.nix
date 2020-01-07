4569bf8172fb3e6093b32637a54f7ced49fe8ed6
Samuel Dionne-Riel <samuel@dionne-riel.com>
2020-01-06 21:24:31 -0500

    Split configuration in a way that can be imported

---
+++ pinebook_pro.nix
@@ -0,0 +1,43 @@
+# This configuration file can be safely imported in your system configuration.
+{ config, pkgs, lib, ... }:
+
+{
+  nixpkgs.overlays = [
+    (import ./overlay.nix)
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_pinebookpro;
+
+  # This list of modules is not entirely minified, but represents
+  # a set of modules that is required for the display to work in stage-1.
+  # Further minification can be done, but requires trial-and-error mainly.
+  boot.initrd.kernelModules = [
+    # Rockchip modules
+    "rockchip_rga"
+    "rockchip_saradc"
+    "rockchip_thermal"
+    "rockchipdrm"
+
+    # GPU/Display modules
+    "analogix_dp"
+    "cec"
+    "drm"
+    "drm_kms_helper"
+    "dw_hdmi"
+    "dw_mipi_dsi"
+    "gpu_sched"
+    "panel_simple"
+    "panfrost"
+    "pwm_bl"
+
+    # USB / Type-C related modules
+    "fusb302"
+    "tcpm"
+    "typec"
+
+    # Misc. modules
+    "cw2015_battery"
+    "gpio_charger"
+    "rtc_rk808"
+  ];
+}
