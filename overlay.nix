fb057b9eca78d2864d5c983136e4a52128deea81
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-05-11 19:05:42 -0400

    u-boot: Remove customized build

--- overlay.nix
+++ overlay.nix
@@ -4,13 +4,6 @@ let
   inherit (final) callPackage kernelPatches linuxPackagesFor;
 in
 {
-  # Alternative BSP u-boot, with nvme support if desired
-  #   * https://gitlab.manjaro.org/manjaro-arm/packages/core/uboot-pinebookpro
-  uBootPinebookPro = callPackage ./u-boot {};
-  uBootPinebookProExternalFirst = callPackage ./u-boot {
-    externalFirst = true;
-  };
-
   # The unqualified kernel attr is deprecated.
   linux_pinebookpro = throw "The linux_pinebookpro attribute has been replaced by linux_pinebookpro_latest.";
   linuxPackages_pinebookpro = throw "The linuxPackages_pinebookpro attribute has been replaced by linuxPackages_pinebookpro_latest.";
