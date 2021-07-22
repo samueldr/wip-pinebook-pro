366b7547d7a68b43c05fcf1a424bb20065693e16
Franz Pletz <fpletz@fnordicwalking.de>
2021-07-22 22:23:33 +0200

    linux_pinebookpro_latest: add megous & some more manjaro patches

--- kernel/latest/default.nix
+++ kernel/latest/default.nix
@@ -11,6 +11,13 @@ let
       inherit sha256;
     });
   };
+  megousPatch = rev: sha256: {
+    name = "megous-linux-${rev}";
+    patch = (fetchpatch {
+      url = "https://github.com/megous/linux/commit/${rev}.diff";
+      inherit sha256;
+    });
+  };
 in
 linux_latest.override({
   kernelPatches = lib.lists.unique (kernelPatches ++ [
@@ -59,7 +66,11 @@ linux_latest.override({
     (manjaroArmPatch "0001-phy-rockchip-typec-Set-extcon-capabilities.patch"                   "0pqq856g0yndxvg9ipbx1jv6j4ldvapgzvxzvpirygc7f0wdrz49")
     (manjaroArmPatch "0002-usb-typec-altmodes-displayport-Add-hacky-generic-altmode.patch"     "1vldwg3zwrx2ppqgbhc91l48nfmjkmwwdsyq6mq6f3l1cwfdn62q")
     (manjaroArmPatch "0003-arm64-dts-rockchip-add-typec-extcon-hack.patch"                     "1kri47nkm6qgsqgkxzgy6iwhpajcx9xwd4rf8dldr6prb9f6iv3p")
-    (manjaroArmPatch "0004-arm64-dts-rockchip-setup-USB-type-c-port-as-dual-data-role.patch"   "0zwwyhryghafga36mgnazn6gk88m2rvs8ng5ykk4hhg9pi5bgzh9")
+    (manjaroArmPatch "0007-nuumio-panfrost-Silence-Panfrost-gem-shrinker-loggin.patch"         "0cj3cdnffr33l4j0cp4ps3jx7b3pyavpqqhb5zdrgi55n5xgw3bg")
+    (manjaroArmPatch "0017-drm-meson-fix-green-pink-color-distortion-set-from-u.patch"         "1scmcjkw3ablm35d2i1g4lrwzhfd7933kr0hcik70y8s8i2b6d5p")
+    (manjaroArmPatch "0019-drm-panfrost-Handle-failure-in-panfrost_job_hw_submit.patch"        "0hblgacdrpwm9s9djiczcj47jv1l6sxys6nzrmjfk2955j1jq0ib")
+    (megousPatch "40cc9599ca2d6fa6d3c0aaf7401cec77787fd39e" "0k1ypac7khwy4703z36rx2daia9bnghanmslzfhk5jlk5hxgqgjs")
+    (megousPatch "520e74f92e259b19052de879d08575155a91055b" "0sngyj7qww9bhra1kg5h68fisrhfkjdfgkzgsp00d75ww5sqlfxs")
   ]);
 })
 //
