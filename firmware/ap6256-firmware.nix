5315d92ce5e62a4ea7a87f6e52f31ad2c8534e58
Zhaofeng Li <hello@zhaofeng.li>
2021-07-24 17:04:19 -0700

    ap6256-firmware: Switch to Manjaro ARM's files which are identical

--- firmware/ap6256-firmware.nix
+++ firmware/ap6256-firmware.nix
@@ -5,8 +5,8 @@
 }:
 
 let
-  nhp = patch: sha256: let rev = "c74b23b8766e4cfc50d1197e6dcd08cc1625866f"; in fetchurl {
-    url = "https://raw.githubusercontent.com/nadiaholmquist/pbp-packages/${rev}/ap6256-firmware/${patch}";
+  manjaroArm = file: sha256: let rev = "7074a2e21dd804e229eab1c031bc00246e9173e0"; in fetchurl {
+    url = "https://gitlab.manjaro.org/manjaro-arm/packages/community/ap6256-firmware/-/raw/${rev}/${file}";
     inherit sha256;
   };
 in
@@ -17,14 +17,13 @@ runCommandNoCC "pinebookpro-ap6256-firmware" {
 } ''
   (PS4=" $ "; set -x
 
-  cp ${(nhp "BCM4345C5.hcd"               "1vl3gkgdqdlhyg9dyflqi6icglr2pll6zr82147g69pfvp6ckv96")} "BCM4345C5.hcd"
-  cp ${(nhp "fw_bcm43456c5_ag.bin"        "03qqgzjz152zaj9y0ppqqsqs03yzi8sb71rfvr29zc1xg1x74y3r")} "fw_bcm43456c5_ag.bin"
-  cp ${(nhp "brcmfmac43456-sdio.clm_blob" "0bi5y3qkqx95c6bb872slw0kig14c453r33j14qyb2f7id8m08lf")} "brcmfmac43456-sdio.clm_blob"
-  cp ${(nhp "nvram_ap6256.txt"            "1zsnswiiwx50pbwl8574xa7z07v9iyfajxccbfrnc8ap99gzpvj3")} "nvram_ap6256.txt"
+  cp ${(manjaroArm "BCM4345C5.hcd"               "1vl3gkgdqdlhyg9dyflqi6icglr2pll6zr82147g69pfvp6ckv96")} "BCM4345C5.hcd"
+  cp ${(manjaroArm "fw_bcm43456c5_ag.bin"        "03qqgzjz152zaj9y0ppqqsqs03yzi8sb71rfvr29zc1xg1x74y3r")} "fw_bcm43456c5_ag.bin"
+  cp ${(manjaroArm "brcmfmac43456-sdio.clm_blob" "0bi5y3qkqx95c6bb872slw0kig14c453r33j14qyb2f7id8m08lf")} "brcmfmac43456-sdio.clm_blob"
+  cp ${(manjaroArm "nvram_ap6256.txt"            "1zsnswiiwx50pbwl8574xa7z07v9iyfajxccbfrnc8ap99gzpvj3")} "nvram_ap6256.txt"
 
   mkdir -p $out/lib/firmware/brcm
 
-  # https://github.com/nadiaholmquist/pbp-packages/blob/ded66e50064c55a56a958558ab35bc6bae444e72/ap6256-firmware/PKGBUILD#L22
   # Bluetooth firmware
   install -Dm644 "BCM4345C5.hcd" -t "$out/lib/firmware/"
   install -Dm644 "BCM4345C5.hcd"    "$out/lib/firmware/brcm/BCM.hcd"
