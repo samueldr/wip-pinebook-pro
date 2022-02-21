a53cef6c6fa83f9736d6c6db82133c77001d2c33
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-02-20 20:12:14 -0500

    ap6256-firmware.nix: Update

--- firmware/ap6256-firmware.nix
+++ firmware/ap6256-firmware.nix
@@ -5,9 +5,12 @@
 }:
 
 let
-  nhp = patch: sha256: let rev = "c74b23b8766e4cfc50d1197e6dcd08cc1625866f"; in fetchurl {
-    url = "https://raw.githubusercontent.com/nadiaholmquist/pbp-packages/${rev}/ap6256-firmware/${patch}";
-    inherit sha256;
+  src = fetchFromGitLab {
+    domain = "gitlab.manjaro.org";
+    owner = "manjaro-arm";
+    repo = "packages/community/ap6256-firmware";
+    rev = "007c6dd132263624db9ea6ce8dde4fc2108f21b7";
+    sha256 = "sha256-Agqz05W7Rhl0NNQwpW4aQqs9YZmYyci9OXKzEKA30uk=";
   };
 in
 runCommandNoCC "pinebookpro-ap6256-firmware" {
@@ -17,14 +20,13 @@ runCommandNoCC "pinebookpro-ap6256-firmware" {
 } ''
   (PS4=" $ "; set -x
 
-  cp ${(nhp "BCM4345C5.hcd"               "1vl3gkgdqdlhyg9dyflqi6icglr2pll6zr82147g69pfvp6ckv96")} "BCM4345C5.hcd"
-  cp ${(nhp "fw_bcm43456c5_ag.bin"        "03qqgzjz152zaj9y0ppqqsqs03yzi8sb71rfvr29zc1xg1x74y3r")} "fw_bcm43456c5_ag.bin"
-  cp ${(nhp "brcmfmac43456-sdio.clm_blob" "0bi5y3qkqx95c6bb872slw0kig14c453r33j14qyb2f7id8m08lf")} "brcmfmac43456-sdio.clm_blob"
-  cp ${(nhp "nvram_ap6256.txt"            "1zsnswiiwx50pbwl8574xa7z07v9iyfajxccbfrnc8ap99gzpvj3")} "nvram_ap6256.txt"
+  cp ${src}/"BCM4345C5.hcd"               "BCM4345C5.hcd"
+  cp ${src}/"fw_bcm43456c5_ag.bin"        "fw_bcm43456c5_ag.bin"
+  cp ${src}/"brcmfmac43456-sdio.clm_blob" "brcmfmac43456-sdio.clm_blob"
+  cp ${src}/"nvram_ap6256.txt"            "nvram_ap6256.txt"
 
   mkdir -p $out/lib/firmware/brcm
 
-  # https://github.com/nadiaholmquist/pbp-packages/blob/ded66e50064c55a56a958558ab35bc6bae444e72/ap6256-firmware/PKGBUILD#L22
   # Bluetooth firmware
   install -Dm644 "BCM4345C5.hcd" -t "$out/lib/firmware/"
   install -Dm644 "BCM4345C5.hcd"    "$out/lib/firmware/brcm/BCM.hcd"
