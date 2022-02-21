9d588b6faaec34207fae8052e850b5efb037ca54
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-02-20 20:21:25 -0500

    Drop now unneeded files

--- system.nix
+++
@@ -1,7 +0,0 @@
-import <nixpkgs/nixos> {
-  configuration =
-    if builtins.currentSystem == "aarch64-linux"
-    then builtins.toPath (./. + "/configuration.nix")
-    else builtins.toPath (./. + "/with-cross.nix")
-  ;
-}
